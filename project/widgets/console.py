cb408599887a3612ed7782a6d58ed2e597007b87
DavidLiuWangli <dlwangli@up.edu.ph>
2024-11-21 14:44:09 +0800

    updated console

--- project/widgets/console.py
+++ project/widgets/console.py
@@ -2,6 +2,7 @@ import tkinter as tk
 from widgets.widget import Widget
 from widgets.ui_elements import create_styled_label, create_styled_button
 from lexical_analyzer.lexical_analyzer import lexical_analyzer
+from syntax_analyzer.syntax_analyzer import syntax_analyzer
 
 class Console(Widget):
     def __init__(self, root, text_editor, tokens_table, symbol_table):
@@ -13,12 +14,35 @@ class Console(Widget):
         self.tokens_table = tokens_table
         self.symbol_table = symbol_table
         
-        # Title and Run Button
+        # Title and Buttons
         title = create_styled_label(self.header, "Console")
         title.pack(side=tk.LEFT)
+        parse_button = create_styled_button(self.header, "Parse", self.parse)
+        parse_button.pack(side=tk.RIGHT)
         run_button = create_styled_button(self.header, "Run", self.run)
         run_button.pack(side=tk.RIGHT)
         
+        # Console Output
+        self.text_area = scrolledtext.ScrolledText(
+            self.content, 
+            fg="white", 
+            font=("Consolas", 12), 
+            bg="black",
+            insertbackground="white",
+            wrap="none",
+            undo=True)
+        tab_size = font.Font(font=self.text_area["font"]).measure("    ")  # Measure 4 spaces
+        self.text_area.config(tabs=(tab_size,))
+        self.text_area.config(state="disabled")
+        self.text_area.pack(fill=tk.BOTH, side=tk.TOP, expand=True)
+    
+    def parse(self):
+        self.text_area.config(state="normal")
+        self.text_area.delete("1.0", tk.END)
+        parse_tree = syntax_analyzer(self.text_editor.text_area.get("1.0", "end-1c"))
+        self.text_area.insert(tk.END, parse_tree)
+        self.text_area.config(state="disabled")
+    
     def run(self):
         tokens = lexical_analyzer(self.text_editor.text_area.get("1.0", "end-1c"))
         self.tokens_table.update(tokens)
